6842e50cb7f71f2612b8f297b4ce7079073aaccc
azizovmukhammad <azizovmuhammaduz@gmail.com>
2022-02-09 09:18:59 +0500

    Initial commit

---
+++ src/test/java/com/example/Warehouse/System/WarehouseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Warehouse.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
